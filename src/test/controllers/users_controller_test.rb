98f2c0b64993922234554accf85593c188de6575
raccoon <toshiki21320@icloud.com>
2022-08-05 00:30:43 +0900

    test設定

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert false
-  # end
+  test "the truth" do
+    assert false
+  end
   # test "should get index" do
   #   get users_index_url
   #   assert_response :success
