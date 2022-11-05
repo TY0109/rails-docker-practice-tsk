0c63c1f767c1e61a698ab7797fbf7d7ab05314bf
raccoon <toshiki21320@icloud.com>
2022-11-05 18:13:54 +0900

    js周りの修正

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,18 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
-//bootstrap導入
-require("bootstrap");
-
-//scss導入
-import "../stylesheets/application.scss";
+import 'bootstrap';
+import '../stylesheets/application';
 
--- src/config/webpack/environment.js
+++ src/config/webpack/environment.js
@@ -1,12 +1,10 @@
 const { environment } = require('@rails/webpacker')
-var webpack = require('webpack');
-
-environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
+const webpack = require('webpack')
+// css, bootstrapなどが適用されなくなったので、4行目を追加したところ復活した
+// environment.loaders.get('sass').use.push('import-glob-loader')
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-        Popper: ['popper.js', 'default']
+        jQuery: 'jquery/src/jquery'
     })
 )
 
