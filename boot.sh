938e34dba80e20fe2fbd00b5a864139574ed1f17
raccoon <toshiki21320@icloud.com>
2022-11-06 18:17:36 +0900

    nginx不要ファイル削除

--- boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# build
-/usr/bin/erb /usr/local/openresty/nginx/conf/nginx.conf.erb > /usr/local/openresty/nginx/conf/nginx.conf
-
-# start nginx
-/usr/local/openresty/nginx/sbin/nginx
