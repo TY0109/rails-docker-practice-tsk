c323ad6cf07017b6cb60d9e075d17f64ba216106
raccoon <toshiki21320@icloud.com>
2022-11-06 12:57:22 +0900

    js系の編集

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery");
+// require("jquery");
 
-import 'bootstrap';
+// import 'bootstrap';
 import '../stylesheets/application';
 
