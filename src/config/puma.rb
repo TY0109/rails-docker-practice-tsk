89711a45a234808540ef5bbf6daad0d858cb624f
raccoon <toshiki21320@icloud.com>
2022-11-07 14:20:32 +0900

    puma rb修正

--- src/config/puma.rb
+++ src/config/puma.rb
@@ -1,10 +1,7 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-plugin :tmp_restart
-
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+# 1 /tmp/nginx.socketでlisten
+bind "unix:///tmp/nginx.socket"
+# 2 masterプロセスがworkerプロセスを生成するときに/tmp/app-initializedを生成する
+require 'fileutils'
+on_worker_fork do
+    FileUtils.touch('/tmp/app-initialized')
+end
